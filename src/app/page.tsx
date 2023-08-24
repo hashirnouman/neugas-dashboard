f1e458e3b1f3b07f5cb5b898dc61087832a8f9a9
hashirnouman <hashirnouman76@gmail.com>
2023-08-24 11:20:44 +0500

    color pallete added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div className="bg-primary-dark-900">
+      <div className="">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
         officia obcaecati alias placeat! Voluptatum id dolor itaque, optio
         suscipit atque sed explicabo soluta eum numquam incidunt fugiat deleniti
